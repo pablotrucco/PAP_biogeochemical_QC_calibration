7b80db572e2c55e40193de4e84bf68acd872160a
Pablo Trucco <pablotrucco@Pablos-MacBook-Air.local>
2024-02-01 14:15:40 +0000

    The function CTD_O2_qc.m has also been updated to account when you only have one oxygen sensor from the CTD. PTP 01/02/2021

--- Functions/CTD_O2_qc.m
+++ Functions/CTD_O2_qc.m
@@ -12,8 +12,8 @@ function cruise_struct=CTD_O2_qc(cruise_struct,sig,acclim_depth,plot_graph)
 %input or leave to the defaul 20 m.
 %
 %Finally you can opt to plot the graphs that shows you the profiles of each
-%sensor (primary and seconday) and the identified outliers (QF=3). 
-% 
+%sensor (primary and seconday) and the identified outliers (QF=3).
+%
 % The flagging scheme followed here obey to those stablished for the Ocean
 %Carbon and Acidification Data System (OCADS) and despite that here is just
 %assessed values as questionable or acceptable, the flagging system is as
@@ -46,124 +46,136 @@ subStructNames = fieldnames(cruise_struct);
 for ii = 1:length(subStructNames)
     % Get the current substructure
     currentSubStruct = cruise_struct.(subStructNames{ii});
-    % Calculate the difference between consecutive values in CTDOXY_umol_kg_1 and CTDOXY_umol_kg_2
-    diff_CTDOXY_1 = diff(currentSubStruct.CTDOXY_umol_kg_1);
-    diff_CTDOXY_2 = diff(currentSubStruct.CTDOXY_umol_kg_2);
-
-    % Identify outliers in diff_CTDSAL_1 using the standard deviation method
-    mu = mean(diff_CTDOXY_1);
-    sigma = std(diff_CTDOXY_1);
-    outlierIndex_CTDOXY_1 = find(diff_CTDOXY_1 < mu - sig*sigma | diff_CTDOXY_1 > mu + sig*sigma);
-    
-    % Identify outliers in diff_CTDSAL_2 using the standard deviation method
-    mu = mean(diff_CTDOXY_2);
-    sigma = std(diff_CTDOXY_2);
-    outlierIndex_CTDOXY_2 = find(diff_CTDOXY_2 < mu - sig*sigma | diff_CTDOXY_2 > mu + sig*sigma);
-    
-    % Create new substructures to store the outlier flags
-    CTDOXY_1_FLAG = ones(size(currentSubStruct.CTDOXY_umol_kg_1)) * 2;
-    CTDOXY_1_FLAG(outlierIndex_CTDOXY_1) = 3;
-
-    CTDOXY_2_FLAG = ones(size(currentSubStruct.CTDOXY_umol_kg_2)) * 2;
-    CTDOXY_2_FLAG(outlierIndex_CTDOXY_2) = 3;
 
     % Flag data above 20 meters or specified acclim_depth in the downcast
     [~, maxIndex] = max(currentSubStruct.CTDPRS);
     downcastStartIndex = find(currentSubStruct.CTDPRS(1:maxIndex) == min(currentSubStruct.CTDPRS(1:maxIndex)), 1, 'last');
     downcastEndIndex = find(currentSubStruct.CTDPRS(1:maxIndex) > acclim_depth, 1, 'first');
-    
-    if ~isempty(downcastStartIndex) && ~isempty(downcastEndIndex)
-        CTDOXY_1_FLAG(downcastStartIndex:downcastEndIndex) = 3;
-        CTDOXY_2_FLAG(downcastStartIndex:downcastEndIndex) = 3;
+
+    % Calculate the difference between consecutive values in CTDOXY_umol_kg_1
+    % and CTDOXY_umol_kg_2 if they exist.
+
+    if isfield(currentSubStruct, 'CTDOXY_umol_kg_1')
+
+        diff_CTDOXY_1 = diff(currentSubStruct.CTDOXY_umol_kg_1);
+        % Identify outliers in diff_CTDSAL_1 using the standard deviation method
+        mu = mean(diff_CTDOXY_1);
+        sigma = std(diff_CTDOXY_1);
+        outlierIndex_CTDOXY_1 = find(diff_CTDOXY_1 < mu - sig*sigma | diff_CTDOXY_1 > mu + sig*sigma);
+
+        % Create new substructures to store the outlier flags
+
+        CTDOXY_1_FLAG = ones(size(currentSubStruct.CTDOXY_umol_kg_1)) * 2;
+        CTDOXY_1_FLAG(outlierIndex_CTDOXY_1) = 3;
+
+        if ~isempty(downcastStartIndex) && ~isempty(downcastEndIndex)
+            CTDOXY_1_FLAG(downcastStartIndex:downcastEndIndex) = 3;
+        end
+        CTDOXY_1_FLAG(end-1:end)=3;
+
+        %Extra quality control measurements rate of change (similar to sigma
+        % but different approach), spike_test and flat line
+        %
+        %The following tests incorporate the guidelines for QC test outlined in:
+        %
+        %QARTOD (2015). Manual for Real-Time Quality Control of Dissolved Oxygen
+        % Observations: A Guide to Quality Control and Quality Assurance for Dissolved
+        % Oxygen Observations in Coastal Oceans. Version 2.0. U.S. Integrated Ocean Observing System.
+        %
+        %Wong, A., Keeley, R., Carval, T., & the Argo Data Management Team (2019).
+        % Argo Quality Control Manual for CTD and Trajectory Data.
+        % Version 3.2. http://dx.doi.org/10.13155/33951
+
+        %Primary sensor
+
+        TimeV=currentSubStruct.Time_elapsed_s;
+        [flag,~]=spike_test(currentSubStruct.CTDOXY_umol_kg_1,'spike_suspect',0.5, ...
+            'spike_fail',2,'time',TimeV);
+        CTDOXY_1_FLAG(ismember(flag,3))=3;
+        CTDOXY_1_FLAG(ismember(flag,4))=4;
+
+        [flag,~]=rate_of_change_test(currentSubStruct.CTDOXY_umol_kg_1, ...
+            'rate_suspect',2,'time',TimeV,'threshold_rate','pertimestep');
+        CTDOXY_1_FLAG(ismember(flag,3))=3;
+        CTDOXY_1_FLAG(ismember(flag,4))=4;
+
+        [flag,~]=flat_line_test(currentSubStruct.CTDOXY_umol_kg_1);
+        CTDOXY_1_FLAG(ismember(flag,3))=3;
+        CTDOXY_1_FLAG(ismember(flag,4))=4;
+
+        assignin('caller', inputname(1), setfield(cruise_struct, subStructNames{ii}, 'CTDOXY_1_FLAG', CTDOXY_1_FLAG));
+        cruise_struct = evalin('caller', inputname(1));
+
     end
-    CTDOXY_1_FLAG(end-1:end)=3;
-    CTDOXY_2_FLAG(end-1:end)=3;
+    %% Seconday sensor, if it exist
 
-    %Extra quality control measurements rate of change (similar to sigma 
-    % but different approach), spike_test and flat line
-    %
-    %The following tests incorporate the guidelines for QC test outlined in:
-    %
-    %QARTOD (2015). Manual for Real-Time Quality Control of Dissolved Oxygen 
-    % Observations: A Guide to Quality Control and Quality Assurance for Dissolved 
-    % Oxygen Observations in Coastal Oceans. Version 2.0. U.S. Integrated Ocean Observing System.
-    %
-    %Wong, A., Keeley, R., Carval, T., & the Argo Data Management Team (2019). 
-    % Argo Quality Control Manual for CTD and Trajectory Data. 
-    % Version 3.2. http://dx.doi.org/10.13155/33951
-    
-    %Primary sensor
- 
-    TimeV=currentSubStruct.Time_elapsed_s;
-    [flag,~]=spike_test(currentSubStruct.CTDOXY_umol_kg_1,'spike_suspect',0.5, ...
-        'spike_fail',2,'time',TimeV);
-    CTDOXY_1_FLAG(ismember(flag,3))=3;
-    CTDOXY_1_FLAG(ismember(flag,4))=4;
+    if isfield(currentSubStruct, 'CTDOXY_umol_kg_2')
 
-    [flag,~]=rate_of_change_test(currentSubStruct.CTDOXY_umol_kg_1, ...
-        'rate_suspect',2,'time',TimeV,'threshold_rate','pertimestep');
-    CTDOXY_1_FLAG(ismember(flag,3))=3;
-    CTDOXY_1_FLAG(ismember(flag,4))=4;
+        diff_CTDOXY_2 = diff(currentSubStruct.CTDOXY_umol_kg_2);
 
-    [flag,~]=flat_line_test(currentSubStruct.CTDOXY_umol_kg_1);
-    CTDOXY_1_FLAG(ismember(flag,3))=3;
-    CTDOXY_1_FLAG(ismember(flag,4))=4;
-
-    %Seconday sensor
- 
-    TimeV=currentSubStruct.Time_elapsed_s;
-    [flag,~]=spike_test(currentSubStruct.CTDOXY_umol_kg_2,'spike_suspect',0.5, ...
-        'spike_fail',2,'time',TimeV);
-    CTDOXY_2_FLAG(ismember(flag,3))=3;
-    CTDOXY_2_FLAG(ismember(flag,4))=4;
-
-    [flag,~]=rate_of_change_test(currentSubStruct.CTDOXY_umol_kg_2, ...
-        'rate_suspect',2,'time',TimeV,'threshold_rate','pertimestep');
-    CTDOXY_2_FLAG(ismember(flag,3))=3;
-    CTDOXY_2_FLAG(ismember(flag,4))=4;
-
-    [flag,~]=flat_line_test(currentSubStruct.CTDOXY_umol_kg_2);
-    CTDOXY_2_FLAG(ismember(flag,3))=3;
-    CTDOXY_2_FLAG(ismember(flag,4))=4;
-
-    assignin('caller', inputname(1), setfield(cruise_struct, subStructNames{ii}, 'CTDOXY_1_FLAG', CTDOXY_1_FLAG));
-    cruise_struct = evalin('caller', inputname(1));
+        mu = mean(diff_CTDOXY_2);
+        sigma = std(diff_CTDOXY_2);
+        outlierIndex_CTDOXY_2 = find(diff_CTDOXY_2 < mu - sig*sigma | diff_CTDOXY_2 > mu + sig*sigma);
 
 
-    assignin('caller', inputname(1), setfield(cruise_struct, subStructNames{ii}, 'CTDOXY_2_FLAG', CTDOXY_2_FLAG));
+        CTDOXY_2_FLAG = ones(size(currentSubStruct.CTDOXY_umol_kg_2)) * 2;
+        CTDOXY_2_FLAG(outlierIndex_CTDOXY_2) = 3;
+
+        if ~isempty(downcastStartIndex) && ~isempty(downcastEndIndex)
+            CTDOXY_2_FLAG(downcastStartIndex:downcastEndIndex) = 3;
+        end
+        CTDOXY_1_FLAG(end-1:end)=3;
+
+        TimeV=currentSubStruct.Time_elapsed_s;
+        [flag,~]=spike_test(currentSubStruct.CTDOXY_umol_kg_2,'spike_suspect',0.5, ...
+            'spike_fail',2,'time',TimeV);
+        CTDOXY_2_FLAG(ismember(flag,3))=3;
+        CTDOXY_2_FLAG(ismember(flag,4))=4;
+
+        [flag,~]=rate_of_change_test(currentSubStruct.CTDOXY_umol_kg_2, ...
+            'rate_suspect',2,'time',TimeV,'threshold_rate','pertimestep');
+        CTDOXY_2_FLAG(ismember(flag,3))=3;
+        CTDOXY_2_FLAG(ismember(flag,4))=4;
+
+        [flag,~]=flat_line_test(currentSubStruct.CTDOXY_umol_kg_2);
+        CTDOXY_2_FLAG(ismember(flag,3))=3;
+        CTDOXY_2_FLAG(ismember(flag,4))=4;
+
+        assignin('caller', inputname(1), setfield(cruise_struct, subStructNames{ii}, 'CTDOXY_2_FLAG', CTDOXY_2_FLAG));
+    end
+
     cruise_struct = evalin('caller', inputname(1));
     currentSubStruct = cruise_struct.(subStructNames{ii});
 
     % Create a new figure for the current substructure
     if plot_graph==1
-        figure;
-        
-        % Plot CTDPRS against CTDSAL_1
-        plot(currentSubStruct.CTDOXY_umol_kg_1, currentSubStruct.CTDPRS);
-        hold on;
-        axis ij
-        % Plot CTDPRS against CTDSAL_2
-        plot(currentSubStruct.CTDOXY_umol_kg_2, currentSubStruct.CTDPRS);
-        
-        ix1=find(ismember(currentSubStruct.CTDOXY_1_FLAG,3));
-        ix2=find(ismember(currentSubStruct.CTDOXY_1_FLAG,4));
-        
-        ix3=find(ismember(currentSubStruct.CTDOXY_2_FLAG,3));
-        ix4=find(ismember(currentSubStruct.CTDOXY_2_FLAG,4));
-        % Plot Questionable data
-        plot([currentSubStruct.CTDOXY_umol_kg_1(ix1);currentSubStruct.CTDOXY_umol_kg_2(ix3)], ...
-            [currentSubStruct.CTDPRS(ix1);currentSubStruct.CTDPRS(ix3)], 'gx','MarkerSize',10,'LineWidth',3);
+        figure
+        hold on
+        if isfield(currentSubStruct, 'CTDOXY_umol_kg_1')
+            % Plot CTDPRS against CTDSAL_1
+            plot(currentSubStruct.CTDOXY_umol_kg_1, currentSubStruct.CTDPRS);
+            ix1=find(ismember(currentSubStruct.CTDOXY_1_FLAG,3));
+            ix2=find(ismember(currentSubStruct.CTDOXY_1_FLAG,4));
+            % Plot Questionable data
+            plot(currentSubStruct.CTDOXY_umol_kg_1(ix1), ...
+                currentSubStruct.CTDPRS(ix1), 'gx','MarkerSize',10,'LineWidth',3);
+            %Plot Bad data
+            plot(currentSubStruct.CTDOXY_umol_kg_1(ix2), ...
+                currentSubStruct.CTDPRS(ix2), 'or','MarkerSize',30,'LineWidth',3);
+        end
 
-        %plot(currentSubStruct.CTDOXY_umol_kg_1(ix1), currentSubStruct.CTDPRS(ix1), 'gx','MarkerSize',10,'LineWidth',3);
-        %plot(currentSubStruct.CTDOXY_umol_kg_2(ix3), currentSubStruct.CTDPRS(ix3), 'gx','MarkerSize',10,'LineWidth',3);
-       
-
-        %Plot Bad data
-        
-        plot([currentSubStruct.CTDOXY_umol_kg_1(ix2);currentSubStruct.CTDOXY_umol_kg_2(ix4)], ...
-            [currentSubStruct.CTDPRS(ix2);currentSubStruct.CTDPRS(ix4)], 'or','MarkerSize',30,'LineWidth',3);
-        %plot(currentSubStruct.CTDOXY_umol_kg_1(ix2), currentSubStruct.CTDPRS(ix2), 'or','MarkerSize',30,'LineWidth',3);
-        %plot(currentSubStruct.CTDOXY_umol_kg_2(ix4), currentSubStruct.CTDPRS(ix4), 'or','MarkerSize',30,'LineWidth',3);
+        if isfield(currentSubStruct, 'CTDOXY_umol_kg_2')
+            % Plot CTDPRS against CTDSAL_2
+            plot(currentSubStruct.CTDOXY_umol_kg_2, currentSubStruct.CTDPRS);
+            ix3=find(ismember(currentSubStruct.CTDOXY_2_FLAG,3));
+            ix4=find(ismember(currentSubStruct.CTDOXY_2_FLAG,4));
+            % Plot Questionable data
+            plot(currentSubStruct.CTDOXY_umol_kg_2(ix3), ...
+                currentSubStruct.CTDPRS(ix3), 'gx','MarkerSize',10,'LineWidth',3);
+            %Plot Bad data
+            plot(currentSubStruct.CTDOXY_umol_kg_2(ix4), ...
+                currentSubStruct.CTDPRS(ix4), 'or','MarkerSize',30,'LineWidth',3);
+        end
 
         % Add labels and title
         xlabel('CTDOXY [\mumol kg^-^1]');
@@ -172,20 +184,41 @@ for ii = 1:length(subStructNames)
         ax=gca;
         ax.FontSize=16;
         axis square; grid on
-        legend_labels = {'Primary sensor', 'Seconday sensor'};
+        axis ij
 
-        % Create a legend only if flagged data points are present
-            if any(CTDOXY_1_FLAG == 3) || any(CTDOXY_1_FLAG == 4) || any(CTDOXY_2_FLAG == 3) || any(CTDOXY_2_FLAG == 4)
-                if any(CTDOXY_1_FLAG == 3) || any(CTDOXY_2_FLAG == 3)
-                    legend_labels{end+1} = 'Questionable data';
-                end
+        % Initialize the legend labels array
+        legend_labels = {};
 
-                if any(CTDOXY_1_FLAG == 4) || any(CTDOXY_2_FLAG == 4)
-                    legend_labels{end+1} = 'Bad data';
-                end
-
+        % Check for the primary sensor
+        if isfield(currentSubStruct, 'CTDOXY_umol_kg_1')
+            legend_labels{end+1} = 'Primary sensor O2';
+            % Check if there are any questionable or bad data points
+            if any(ismember(currentSubStruct.CTDOXY_1_FLAG, 3))
+                legend_labels{end+1} = 'Questionable O2 (Primary)';
             end
-                legend(legend_labels, 'Interpreter', 'none');
+            if any(ismember(currentSubStruct.CTDOXY_1_FLAG, 4))
+                legend_labels{end+1} = 'Bad O2 (Primary)';
+            end
+        end
+
+        % Check for the secondary sensor
+        if isfield(currentSubStruct, 'CTDOXY_umol_kg_2')
+            legend_labels{end+1} = 'Secondary sensor O2';
+            % Check if there are any questionable or bad data points
+            if any(ismember(currentSubStruct.CTDOXY_2_FLAG, 3))
+                legend_labels{end+1} = 'Questionable O2 (Secondary)';
+            end
+            if any(ismember(currentSubStruct.CTDOXY_2_FLAG, 4))
+                legend_labels{end+1} = 'Bad O2 (Secondary)';
+            end
+        end
+
+        % Apply the legend if there are any labels to display
+        if ~isempty(legend_labels)
+            legend(legend_labels, 'Location', 'best');
+        end
+
+
     end
 end
 
